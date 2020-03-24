3fd7bd2e4504608a6ad7fc6ddee0ce90fea284d9
Rafael Souza <raffaelsouza908@gmail.com>
2020-03-24 11:12:18 -0300

    Configurando Knex e criando as migrations

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
 
+import routes from './routes';
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.json({ message: 'bora corno' });
-});
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3333);
