4787324c4aa06a31e467f6d5e2f2cbc46a06a17a
Rafael Souza <raffaelsouza908@gmail.com>
2020-03-25 17:46:48 -0300

    Estilizando pagina de cadastro de caso

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
 
-import './styles';
+import './styles.css';
+
+import logoImg from '../../assets/logo.svg';
 
 export default function NewIncident() {
   return (
-    <div>
-      <h1>Oi</h1>
+    <div className="new-incident-container">
+      <div className="content">
+        <section>
+          <img src={logoImg} alt="Be The Hero" />
+          <h1>Cadastrar novo caso</h1>
+          <p>
+            Descreva o caso detalhadamente para encontrar um herói para resolver
+            isso.
+          </p>
+
+          <Link className="back-link" to="/profile">
+            <FiArrowLeft size={16} color="#e02041" />
+            Voltar para home
+          </Link>
+        </section>
+
+        <form action="">
+          <input type="text" placeholder="Título do caso" />
+          <textarea placeholder="Descrição" />
+          <input type="text" placeholder="Valor em reais" />
+
+          <button className="button" type="submit">
+            Cadastrar
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
+import NewIncident from './pages/NewIncident';
 
 export default function Routes() {
   return (
@@ -12,6 +13,7 @@ export default function Routes() {
         <Route path="/" exact component={Login} />
         <Route path="/register" exact component={Register} />
         <Route path="/profile" exact component={Profile} />
+        <Route path="/incidents/new" exact component={NewIncident} />
       </Switch>
     </BrowserRouter>
   );
