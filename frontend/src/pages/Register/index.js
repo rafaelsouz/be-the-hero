e18c57ead0ebebd283c145bc6d27c8bf6e2c9ed2
Rafael Souza <raffaelsouza908@gmail.com>
2020-03-25 15:50:17 -0300

    Tela de cadastro

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
 
 // import { Container } from './styles';
 
 export default function Register() {
-  return <h1>Register</h1>;
+  return (
+    <div className="register-container">
+      <div className="content">
+        <section>
+          <img src={logoImg} alt="Be The Hero" />
+          <h1>Cadastro</h1>
+          <p>
+            Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem
+            os casos da sua ONG.
+          </p>
+
+          <Link className="back-link" to="/register">
+            <FiArrowLeft size={16} color="#e02041" />
+            Não tenho cadastro
+          </Link>
+        </section>
+
+        <form action="">
+          <input type="text" placeholder="Nome da ONG" />
+          <input type="email" placeholder="Email" />
+          <input type="text" placeholder="Whatsapp" />
+
+          <div>
+            <input type="text" placeholder="Cidade" />
+            <input type="text" placeholder="UF" style={{ width: 80 }} />
+          </div>
+
+          <button className="button" type="submit">
+            Cadastrar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
