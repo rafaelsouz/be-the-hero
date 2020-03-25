742acd86b69ca9e8866a73e95dbbce6a33d8eeaf
Rafael Souza <raffaelsouza908@gmail.com>
2020-03-25 17:07:35 -0300

    Estilização da pagina de casos

---
+++ frontend/src/pages/NewIncident/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './styles';
+
+export default function NewIncident() {
+  return (
+    <div>
+      <h1>Oi</h1>
+    </div>
+  );
+}
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
 
 export default function Routes() {
   return (
@@ -10,6 +11,7 @@ export default function Routes() {
       <Switch>
         <Route path="/" exact component={Login} />
         <Route path="/register" exact component={Register} />
+        <Route path="/profile" exact component={Profile} />
       </Switch>
     </BrowserRouter>
   );
