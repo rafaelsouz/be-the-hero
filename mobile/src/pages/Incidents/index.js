26dac0154c71cac4b033000684b89eab33078f6d
Rafael Souza <raffaelsouza908@gmail.com>
2020-03-27 14:42:15 -0300

    Iniciando a criação das paginas

---
+++ mobile/src/pages/Incidents/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+
+import logoImg from '../../assets/logo.png';
+
+ import styles from './styles';
+
+export default function Incidents() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logoImg} />
+        <Text style={styles.headerText}>
+          Total de <Text style={styles.headerTextBold}>0 casos</Text>.
+        </Text>
+      </View>
+
+      <Text style={styles.title}>Bem Vindo!</Text>
+      <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia.</Text>
+    </View>
+  );
+}
