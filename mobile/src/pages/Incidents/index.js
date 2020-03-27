16ddccbcef4c1ec446c62460e949c832f79f9321
Rafael Souza <raffaelsouza908@gmail.com>
2020-03-27 15:21:36 -0300

    Pagina de casos criada e estilizida

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import { Feather } from '@expo/vector-icons'
+import {useNavigation} from '@react-navigation/native'
+import { View, FlatList, Image, Text, TouchableOpacity } from 'react-native';
 
 import logoImg from '../../assets/logo.png';
 
  import styles from './styles';
 
 export default function Incidents() {
+  const navigation = useNavigation();
+
+  function navigateToDetail() {
+    navigation.navigate('Detail');
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -17,6 +25,33 @@ export default function Incidents() {
 
       <Text style={styles.title}>Bem Vindo!</Text>
       <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia.</Text>
+
+      <FlatList 
+        data={[1,2,3,4,5]}
+        style={styles.incidentList}
+        keyExtractor={incident => String(incident)}
+        showsVerticalScrollIndicator={false}
+        renderItem={ () => (
+          <View style={styles.incident}>
+
+          <Text style={styles.incidentProperty}>ONG:</Text>
+          <Text style={styles.incidentValue}>APAD</Text>
+
+          <Text style={styles.incidentProperty}>CASO:</Text>
+          <Text style={styles.incidentValue}>Cadelinha atropelada</Text> 
+
+          <Text style={styles.incidentProperty}>Valor:</Text>
+          <Text style={styles.incidentValue}>R$120,00</Text> 
+
+          <TouchableOpacity style={styles.detailsButton} onPress={navigateToDetail}>
+            <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+
+            <Feather name="arrow-right" size={16} color="#e02041" />
+          </TouchableOpacity>
+
+        </View>
+        )}
+      />
     </View>
   );
 }
