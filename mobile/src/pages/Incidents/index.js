726eb0edcf3781353c286fe6abdc7c430961df1f
Rafael Souza <raffaelsouza908@gmail.com>
2020-03-27 19:11:35 -0300

    Carregamento de mais casos baseado na posição do scroll

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -12,6 +12,8 @@ import logoImg from '../../assets/logo.png';
 export default function Incidents() {
   const [incidents, setIncidents] = useState([]);
   const [total, setTotal] = useState(0);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
 
   const navigation = useNavigation();
 
@@ -20,10 +22,23 @@ export default function Incidents() {
   }
 
   async function loadIncidents() {
-    const response = await api.get('incidents');
+    if (loading) {
+      return;
+    }
 
-    setIncidents(response.data)
+    if(total > 0 && incidents.length === total) {
+      return;
+    }
+
+    setLoading(true);
+
+    const response = await api.get('incidents', { params: {page}});
+
+    
+    setIncidents([...incidents, ...response.data]);
     setTotal(response.headers['x-total-count'])
+    setPage(page+1);
+    setLoading(false);
   }
 
   useEffect( () => {
@@ -47,6 +62,8 @@ export default function Incidents() {
         style={styles.incidentList}
         keyExtractor={incident => String(incident.id)}
         showsVerticalScrollIndicator={false}
+        onEndReached={loadIncidents}
+        onEndReachedThreshold={0.2}
         renderItem={( { item: incident } ) => (
           <View style={styles.incident}>
 
